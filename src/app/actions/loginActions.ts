829e7006dbe7fe85eb05bd34261717d6c6e758ff
Virginia Josefina Frias <virginiafriasunc2000@gmail.com>
2025-05-20 01:32:05 +0200

    init: created some test server actions

---
+++ src/app/actions/loginActions.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+export const getAccountInfo = async () => {
+  try {
+    const response = await axios.get(
+      "https://digitalmoney.digitalhouse.com/api/account",
+      {
+        headers: {
+          api_key: process.env.TOKEN,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error al obtener la cuenta:", error);
+    throw error;
+  }
+};
+
+export const getLoginToken = async () => {
+  try {
+    const response = await axios.post(process.env.BASE_URL + "/api/login", {
+      email: process.env.EMAIL_TEST,
+      password: process.env.PASSWORD_TEST,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error al hacer login:", error);
+    throw error;
+  }
+};
+
+export const pingAPI = async () => {
+  try {
+    const response = await axios.get(process.env.BASE_URL + "/ping");
+
+    return response.data;
+  } catch (error) {
+    console.error("Error al hacer ping:", error);
+    throw error;
+  }
+};
